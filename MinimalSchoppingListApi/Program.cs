b6d962163644691d87ace6bf5899e394f4e45c68
ZengaOO <108860031+ZengaOO@users.noreply.github.com>
2022-09-13 15:38:38 +0300

    Get by Id

--- MinimalSchoppingListApi/Program.cs
+++ MinimalSchoppingListApi/Program.cs
@@ -12,6 +12,17 @@ var app = builder.Build();
 app.MapGet("/shoppinglist", async (ApiDbContext db) =>
     await db.Groseries.ToListAsync());
 
+app.MapGet("/shoppinglist/{id}", async (int id, ApiDbContext db) =>
+{
+
+    var grocery = await db.Groseries.FindAsync(id);
+
+    return grocery != null? Results.Ok(grocery) : Results.NotFound();
+
+});
+
+
+
 app.MapPost("/shoppinglist", async (Grocery grocery, ApiDbContext db) =>
 {
     db.Groseries.Add(grocery);
